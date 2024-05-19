04a8ab63ae6add6695a67c8f065a434583d53cde
MRossa <maxim.rossa@mail.ru>
2024-05-19 15:23:18 +0400

    Fixing constants for sandbox

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,7 @@
 RATINGS_PATH = r'MovieLens_20M_Dataset/rating.csv'
 MOVIE_PATH = r'MovieLens_20M_Dataset/movie.csv'
 WEIGHTS_PATH = r'src/weights'
+
+
+RATINGS_PATH_SANDBOX = r'../MovieLens_20M_Dataset/rating.csv'
+MOVIE_PATH_SANDBOX = r'../MovieLens_20M_Dataset/movie.csv'
